59e20afafda1c395d6d69e13049f180b5d2a158a
darkborderman <reastw1234@gmail.com>
2019-04-15 01:49:14 +0800

    Implement showing portait details
    Connect with others' code to implement showing portait details

--- static/js/portait.js
+++ static/js/portait.js
@@ -1,31 +1,82 @@
 const app = new PIXI.Application({ backgroundColor: 0x1099bb });
-//document.body.appendChild(app.view);
 
-document.getElementById("pixi-canvas").appendChild(app.view);
-
-
+//Need to ENSURE the full document DOM and CSS tree is finished then paint
+$(document).ready(()=>{
+    document.getElementById("pixi-canvas").appendChild(app.view);
 const parent = document.getElementById("pixi-canvas");
-console.log(parent);
-   
+
 // Resize the renderer
 app.renderer.resize(parent.clientWidth, parent.clientHeight);
 
 // create a new Sprite from an image path
-const bunny = PIXI.Sprite.from('examples/assets/bunny.png');
 
-// center the sprite's anchor point
-bunny.anchor.set(0.5);
+let bunnies = [];
+let toggleCharacter=0;
+for (let i = 0; i <= 4; i++) {
+    
+    //initialize each character
+    let bunny = PIXI.Sprite.from('../img/bunny.png');
+    bunny.anchor.set(0.5);
+    bunny.width=100;
+    bunny.height=400;
+    bunny.x = app.screen.width / 6 +150*i;
+    bunny.y = app.screen.height - 250;
 
-// move the sprite to the center of the screen
-bunny.x = app.screen.width / 2;
-bunny.y = app.screen.height / 2;
+    //for events
+    bunny.interactive = true;
+    bunny.buttonMode = true;
+    bunny.on('pointerdown', showDetail);
 
-app.stage.addChild(bunny);
+    bunnies.push(bunny);
+    app.stage.addChild(bunny);
+
+}
+
+for(let item in bunnies){
+}
+
 
 // Listen for animate update
 app.ticker.add((delta) => {
-    // just for fun, let's rotate mr rabbit a little
-    // delta is 1 if running at 100% performance
-    // creates frame-independent transformation
-    bunny.rotation += 0.1 * delta;
+});
+
+function showDetail(){
+    i=toggleCharacter;
+    console.log(`here`);
+    if (i == 0) {
+       $("div.top").animate({ top: "-=49%", opacity: 1 }, 500, function () {
+          $("div.pic").animate({ top: "-=35%", opacity: 1 }, 1000);
+          $("div.name").animate({ top: "-=17%", opacity: 1 }, 1000);
+       });
+       $("div.bottom").animate({ top: "+=49%", opacity: 1 }, 500, function () {
+          $("div.brief").animate({ top: "+=14%", opacity: 1 }, 1000);
+       });
+ 
+       $("div.name").html('Tsai Han Ju/')
+       $("div.pic").css("background-image", "url(https://i.imgur.com/dnYu2CZ.png)");
+       $('div.brief').html('Experience/ <br><br>2017 Industrial Design Winter Exhibtion coutor<br>2017 Summer Internship in Gecko Design<br><br><br>Contact/<br><br>abby123445@gmail.com<br>0911-314-875')
+    }
+ 
+    if (i == 1) {
+ 
+       $("div.top").animate({ top: "+=49%", opacity: 0 }, 100)
+          .animate({ top: "-=49%", opacity: 1 }, 500);
+ 
+       $("div.bottom").animate({ top: "-=49%", opacity: 0 }, 100)
+          .animate({ top: "+=49%", opacity: 1 }, 500);
+ 
+       $("div.pic").animate({ top: "+=35%", opacity: 0 }, 200)
+          .animate({ top: "-=35%", opacity: 1 }, 1000);
+       $("div.name").animate({ top: "+=17%", opacity: 0 }, 200)
+          .animate({ top: "-=17%", opacity: 1 }, 1000);
+       $("div.brief").animate({ top: "-=14%", opacity: 0 }, 200)
+          .animate({ top: "+=14%", opacity: 1 }, 1000);
+ 
+ 
+       $("div.name").html('Tsai Han Ju/')
+       $("div.pic").css("background-image", "url(https://i.imgur.com/dnYu2CZ.png)");
+       $('div.brief').html('Experience/ <br><br>2017 Industrial Design Winter Exhibtion coutor<br>2017 Summer Internship in Gecko Design<br><br><br>Contact/<br><br>abby123445@gmail.com<br>0911-314-875')
+    }
+    toggleCharacter = 1;
+ }
 });

8901324428742dc814f3f0d6dd3ccebe970fefe0
darkborderman <reastw1234@gmail.com>
2019-04-15 21:21:07 +0800

    Update picture and align members
    Finish aligning members and upload new pics for unblocking interactions

--- static/js/portait.js
+++ static/js/portait.js
@@ -11,28 +11,50 @@ $(document).ready(() => {
     let members = [
         {
             location: `../img/singleportait1.png`,
+            width:309,
+            height:550,
+            x: 150,
+            y: 300,
             name: 'Tsai Han Ju/',
             brief: 'Experience/ <br><br>2017 Industrial Design Winter Exhibtion coutor<br>2017 Summer Internship in Gecko Design<br><br>Contact/<br><br>abby123445@gmail.com<br>0911-314-875'
         },
         {
             location: `../img/singleportait2.png`,
-            name:'2',
-            brief:'2 profile'
+            width:150,
+            height:550,
+            x: 350,
+            y: 300,
+            name: '2',
+            brief: '2 profile'
         },
         {
             location: `../img/singleportait3.png`,
-            name:'3',
-            brief:'3 profile'
+            width:166,
+            height:550,
+            x: 500,
+            y: 300,
+            name: '3',
+            brief: '3 profile'
         },
         {
             location: `../img/singleportait4.png`,
-            name:'4',
-            brief:'4 profile'
+            width:200,
+            height:550,
+
+            x: 620,
+            y: 300,
+            name: '4',
+            brief: '4 profile'
         },
         {
             location: `../img/singleportait5.png`,
-            name:'5',
-            brief:'5 profile'
+            width:258,
+            height:550,
+
+            x: 800,
+            y: 300,
+            name: '5',
+            brief: '5 profile'
         },
     ]
 
@@ -44,15 +66,18 @@ $(document).ready(() => {
         //initialize each character
         let member = PIXI.Sprite.from(members[i].location);
         member.anchor.set(0.5);
-        member.width=800;
-        member.height=600;
-        member.x=400;
-        member.y=300;
+        //member.scale=0.5;
+        member.width=members[i].width;
+        member.height=members[i].height;
+        member.x = members[i].x;
+        member.y = members[i].y;
+
         //for events
         member.interactive = true;
         member.buttonMode = true;
-        member.on('pointerdown', (object) => { showDetail(object,members, i); });
+        member.on('pointerdown', (object) => { showDetail(object, members, i); });
 
+        console.log(member);
         bunnies.push(member);
         app.stage.addChild(member);
 
@@ -62,7 +87,7 @@ $(document).ready(() => {
     app.ticker.add((delta) => {
     });
 
-    function showDetail(object,members, index) {
+    function showDetail(object, members, index) {
         i = toggleCharacter;
         if (i == 0) {
             $("div.top").animate({ top: "-=49%", opacity: 1 }, 500, function () {
@@ -93,4 +118,39 @@ $(document).ready(() => {
         $('div.brief').html(members[index].brief);
         toggleCharacter = 1;
     }
+
+    disable_scroll();
 });
+
+
+//disable scrolling to prevent our ugly code
+// Writing bad code let me feel guilty, should I blame?
+var keys = [32,33,34,35,36,37,38,39,40];
+
+function preventDefault(e) {
+  e = e || window.event;
+  if (e.preventDefault)
+      e.preventDefault();
+  e.returnValue = false;  
+}
+
+function keydown(e) {
+    for (var i = keys.length; i--;) {
+        if (e.keyCode === keys[i]) {
+            preventDefault(e);
+            return;
+        }
+    }
+}
+
+function wheel(e) {
+  preventDefault(e);
+}
+
+function disable_scroll() {
+  if (window.addEventListener) {
+      window.addEventListener('DOMMouseScroll', wheel, false);
+  }
+  window.onmousewheel = document.onmousewheel = wheel;
+  document.onkeydown = keydown;
+}

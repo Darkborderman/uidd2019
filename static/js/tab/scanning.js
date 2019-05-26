5c1efee95f0f17180da46a0cf59875f163792c5f
darkborderman <reastw1234@gmail.com>
2019-05-26 15:43:58 +0800

    Add OCR, text highlight features

---
+++ static/js/tab/scanning.js
@@ -0,0 +1,25 @@
+$(document).ready(function () {
+    const { TesseractWorker } = Tesseract;
+    console.log(TesseractWorker);
+    const worker = new TesseractWorker();
+    worker.recognize(document.querySelector("img"),'chi_tra')
+        .then(function(data){
+            console.log(data);
+            document.getElementById("paragraph").innerText=data.text;
+        })
+    $(document).on('mouseup', function (e) {
+
+        let x = window.getSelection();
+        let xrange = x.getRangeAt(0);
+        highlightRange(xrange);
+    });
+});
+
+function highlightRange(range) {
+    var newNode = document.createElement("div");
+    newNode.setAttribute(
+       "style",
+       "background-color: yellow; display: inline;"
+    );
+    range.surroundContents(newNode);
+}

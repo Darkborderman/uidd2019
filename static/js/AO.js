e011dc50aab54d3f000cd110a7180f64fc10707a
darkborderman <reastw1234@gmail.com>
2019-06-18 17:57:47 +0800

    Add anchor to each AO

--- static/js/AO.js
+++ static/js/AO.js
@@ -67,3 +67,27 @@ $('#btn1').click(function(){
 
 
 });
+
+$(function(){
+ 
+  // 偵聽a click
+  $(".me_buy a").bind("click",function(){
+
+      var id=$(this).attr("data-id");
+
+      // 取得目標區塊的y座標
+      var target_top = $("#area"+id).offset().top;
+
+      // 取得body物件 這種寫法在各瀏覽器上最保險
+      var $body = (window.opera) ? (document.compatMode == "CSS1Compat" ? $('html') : $('body')) : $('html,body');
+
+      // 移動捲軸無動畫效果
+           //$body.scrollTop(target_top);
+
+      // 移動捲軸有動畫效果
+           $body.animate({
+                 scrollTop: target_top
+           }, 800);
+
+  })
+})
